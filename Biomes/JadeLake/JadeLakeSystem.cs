dd00b6e59fd8f082dc731fc3467baa118a0cf590
GraydeeSi <wiseguy0002@gmail.com>
2022-11-28 20:14:07 -0500

    T

--- Biomes/JadeLake/JadeLakeSystem.cs
+++ Biomes/JadeLake/JadeLakeSystem.cs
@@ -23,7 +23,7 @@ namespace JadeFables.Biomes.JadeLake
                             Tile tile = Main.tile[x, y];
                             if (tile.LiquidAmount > 0 && Main.rand.NextBool(30) && Main.tile[x, y - 1].LiquidAmount <= 0 && !Main.tile[x, y - 1].HasTile)
                             {
-                                for (int i = 0; i < 3; i++)
+                                for (int i = 0; i < 2; i++)
                                 {
                                     Vector2 velocity = Vector2.UnitY.RotatedByRandom(0.1f) * -Main.rand.NextFloat(1f, 1.5f);
                                     Dust.NewDustPerfect(new Vector2(x * 16 + Main.rand.Next(-8, 8), y * 16 + 8), DustID.Smoke, velocity, 220, default, Main.rand.NextFloat(1f, 2f));
@@ -34,10 +34,10 @@ namespace JadeFables.Biomes.JadeLake
                             if (tile.LiquidAmount > 0)
                             {
                                 Lighting.AddLight(new Vector2(x * 16, y * 16), new Vector3(0, 220, 200) * 0.002f);
-                                if (Main.rand.NextBool(500))
+                                if (Main.rand.NextBool(800))
                                     Dust.NewDustPerfect(new Vector2(x * 16, y * 16 + 8), DustType<Dusts.WhiteSparkle>(), Vector2.Zero, 0, default, 0.5f);
 
-                                if (Main.rand.NextBool(500))
+                                if (Main.rand.NextBool(800))
                                     Dust.NewDustPerfect(new Vector2(x * 16, y * 16 + 8), DustType<Dusts.JadeBubble>(), -Vector2.UnitY.RotatedByRandom(0.5f), 0, default, Main.rand.NextFloat(0.75f, 1.5f));
                             }
                         }

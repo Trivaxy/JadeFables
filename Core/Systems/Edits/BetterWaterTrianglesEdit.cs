730fa3e4f2c8ba3a3db4cb8d443b569e9aec9b96
Tomat <xxlennygamerxx@gmail.com>
2022-11-29 20:01:09 -0800

    Incomplete detour as well.

--- Core/Systems/Edits/BetterWaterTrianglesEdit.cs
+++ Core/Systems/Edits/BetterWaterTrianglesEdit.cs
@@ -1,14 +1,27 @@
-﻿using Mono.Cecil.Cil;
+﻿using System.Reflection;
+using Mono.Cecil.Cil;
 using MonoMod.Cil;
+using Terraria.GameContent;
+using Terraria.GameContent.Liquid;
 
 namespace JadeFables.Core.Systems.Edits;
 
 public sealed class BetterWaterTrianglesEdit : RuntimeDetourModSystem
 {
+    private FieldInfo? animationFrameField;
+
     public override void OnModLoad() {
         base.OnModLoad();
 
         IL.Terraria.Main.DrawBlack += InjectExtraDrawBlockConditions;
+
+        animationFrameField = typeof(LiquidRenderer).GetField("_animationFrame", BindingFlags.Instance | BindingFlags.NonPublic);
+        if (animationFrameField is null) {
+            AddMissingMemberError(typeof(LiquidRenderer).FullName + "::_animationFrame");
+            return;
+        }
+
+        On.Terraria.GameContent.Drawing.TileDrawing.DrawPartialLiquid += RenderRealLiquidInPlaceOfPartial;
     }
 
     private void InjectExtraDrawBlockConditions(ILContext il) {
@@ -67,5 +80,80 @@ public sealed class BetterWaterTrianglesEdit : RuntimeDetourModSystem
                || Framing.GetTileSafely(x + 1, y).LiquidAmount > 0
                || Framing.GetTileSafely(x - 1, y).LiquidAmount > 0
                || (Framing.GetTileSafely(x, y).IsHalfBlock && Framing.GetTileSafely(x - 1, y).LiquidAmount > 0 && Lighting.Brightness(x, y) > 0f);
+    }
+
+    private void RenderRealLiquidInPlaceOfPartial(
+        On.Terraria.GameContent.Drawing.TileDrawing.orig_DrawPartialLiquid orig,
+        Terraria.GameContent.Drawing.TileDrawing self,
+        Tile tileCache,
+        Vector2 position,
+        Rectangle liquidSize,
+        int liquidType,
+        Color aColor
+    ) {
+        if (TileID.Sets.BlocksWaterDrawingBehindSelf[(int) tileCache.BlockType] || tileCache.Slope == SlopeType.Solid) {
+            orig(self, tileCache, position, liquidSize, liquidType, aColor);
+            return;
+        }
+
+        var drawPos = Main.drawToScreen ? Vector2.Zero : new Vector2(Main.offScreenRange);
+        var drawOffset = drawPos - Main.screenPosition;
+        var tileCoords = (position + Main.screenPosition - drawPos).ToTileCoordinates();
+
+        Main.tileBatch.Begin();
+
+        Main.DrawTileInWater(drawOffset, tileCoords.X, tileCoords.Y);
+
+        var tile = Framing.GetTileSafely(tileCoords);
+        int liquidStyle = tile.LiquidType;
+        float opacity = tile.LiquidType switch
+        {
+            LiquidID.Water => 0.6f,
+            LiquidID.Lava => 0.95f,
+            LiquidID.Honey => 0.95f,
+            _ => 1f
+        };
+
+        switch (tile.LiquidType) {
+            case LiquidID.Water:
+                liquidStyle = Main.waterStyle;
+                opacity *= 0.75f;
+                break;
+
+            case LiquidID.Honey:
+                liquidStyle = WaterStyleID.Honey;
+                break;
+        }
+
+        opacity = Math.Min(opacity, 1f);
+        if (tile.WallType != 0) opacity = 0.5f;
+
+        Lighting.GetCornerColors(tileCoords.X, tileCoords.Y, out var vertices);
+        vertices.BottomLeftColor *= opacity;
+        vertices.BottomRightColor *= opacity;
+        vertices.TopLeftColor *= opacity;
+        vertices.TopRightColor *= opacity;
+
+        Main.DrawTileInWater(drawOffset, tileCoords.X, tileCoords.Y);
+
+        int frameYOffset = tile.LiquidAmount == 0 && !tile.HasTile ? 0 : 48;
+        var srcRect = new Rectangle(
+            16,
+            frameYOffset + (int) (animationFrameField!.GetValue(LiquidRenderer.Instance) ?? 0) * 80,
+            16,
+            16
+        );
+
+        Main.tileBatch.Draw(
+            TextureAssets.Liquid[liquidStyle].Value,
+            position + new Vector2(0f, tile.IsHalfBlock ? 8f : 0f),
+            srcRect,
+            vertices,
+            Vector2.Zero,
+            1f,
+            SpriteEffects.None
+        );
+
+        Main.tileBatch.End();
     }
 }

f7a0bcffa9c52b28f63602583ac62cc265aa4b3b
GraydeeSi <wiseguy0002@gmail.com>
2022-12-08 17:30:26 -0500

    Dragonflies now spawn

---
+++ NPCs/JadeSpawnPool.cs
@@ -0,0 +1,33 @@
+﻿using JadeFables.Biomes.JadeLake;
+using JadeFables.Dusts;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.Graphics.Effects;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+using static Terraria.ModLoader.PlayerDrawLayer;
+
+namespace JadeFables.NPCs
+{
+    public class JadeSpawnPool : GlobalNPC
+    {
+        public override void EditSpawnPool(IDictionary<int, float> pool, NPCSpawnInfo spawnInfo)
+        {
+            if (spawnInfo.Player.InModBiome(ModContent.GetInstance<JadeLakeBiome>())) 
+            {
+                pool[NPCID.GreenDragonfly] = 75f;
+                pool[NPCID.RedDragonfly] = 75f;
+                pool[NPCID.BlueDragonfly] = 75f;
+                pool[NPCID.YellowDragonfly] = 75f;
+            }
+        }
+    }
+}

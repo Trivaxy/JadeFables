7b9b8e3d7cf1110c40a0a2e62453fd902cb9813f
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2022-12-11 18:54:16 -0800

    Jade Fountain Enable/Disable

--- Tiles/JadeFountain/JadeFountain.cs
+++ Tiles/JadeFountain/JadeFountain.cs
@@ -15,13 +15,14 @@ namespace JadeFables.Tiles.JadeFountain
         {
             Main.tileFrameImportant[Type] = true;
             Main.tileSolid[Type] = false;
-            TileObjectData.newTile.Height = 3;
+            TileObjectData.newTile.Height = 4;
             TileObjectData.newTile.Width = 3;
-            TileObjectData.newTile.Origin = new Point16(0, 2); // Todo: make less annoying.
+            TileObjectData.newTile.Origin = new Point16(1, 2); // Todo: make less annoying.
             TileObjectData.newTile.AnchorBottom = new AnchorData(AnchorType.SolidTile | AnchorType.SolidWithTop | AnchorType.SolidSide, TileObjectData.newTile.Width, 0);
             TileObjectData.newTile.UsesCustomCanPlace = true;
             TileObjectData.newTile.LavaDeath = false;
-            TileObjectData.newTile.CoordinateHeights = new int[] { 16, 16, 16 };
+            TileObjectData.newTile.CoordinateHeights = new int[] { 16, 16, 16, 16 };
+            TileObjectData.newTile.StyleHorizontal = true;
             TileObjectData.newTile.CoordinateWidth = 16;
             TileObjectData.newTile.CoordinatePadding = 2;
             TileObjectData.addTile(Type);
@@ -37,18 +38,44 @@ namespace JadeFables.Tiles.JadeFountain
         public override void KillMultiTile(int i, int j, int frameX, int frameY)
         {
             SoundEngine.PlaySound(SoundID.Item27, new Vector2(i * 16, j * 16));
+
+            Item.NewItem(new EntitySource_TileBreak(i, j), i * 16, j * 16, 3 * 16, 4 * 16, ItemType<JadeFountainItem>());
         }
 
-        public override void AnimateTile(ref int frame, ref int frameCounter)
-        {
-            frameCounter++;
-            if (frameCounter >= 9)
-            {
-                frameCounter = 0;
-                if (++frame >= 4)
-                {
-                    frame = 1;
+        public override bool RightClick(int i, int j) {
+            short pixelWidthOfFullTile = 3 * 18;
+            Tile tile = Main.tile[i, j];
+            int frameXDisplacement = tile.TileFrameX >= pixelWidthOfFullTile ? (int)(tile.TileFrameX / (float)pixelWidthOfFullTile) * pixelWidthOfFullTile : 0;
+            Point topLeft = new Point(i - (tile.TileFrameX - frameXDisplacement) / 18, j - tile.TileFrameY / 18);
+
+            for (int x = 0; x < 3; x++) {
+                for (int y = 0; y < 4; y++) {
+                    Tile newTile = Main.tile[topLeft.X + x, topLeft.Y + y];
+                    newTile.TileFrameX += (short)(newTile.TileFrameX < 18 * 3 ? pixelWidthOfFullTile : -pixelWidthOfFullTile);
                 }
+            }
+            Main.tileFrame[Type] = Main.tileFrameCounter[Type] = 0;
+
+            SoundEngine.PlaySound(SoundID.MenuTick, new Vector2(i * 16, j * 16));
+            return true;
+        }
+
+        public override void AnimateIndividualTile(int type, int i, int j, ref int frameXOffset, ref int frameYOffset) {
+            Tile tile = Main.tile[i, j];
+
+            //Only animate when activated
+            if (tile.TileFrameX == 3 * 18 && tile.TileFrameY % 4 * 18 == 0) {
+                ref int frame = ref Main.tileFrame[type];
+                ref int frameCounter = ref Main.tileFrameCounter[Type];
+
+                frameCounter++;
+                if (frameCounter >= 9) {
+                    frameCounter = 0;
+                    if (++frame >= 3) {
+                        frame = 0;
+                    }
+                }
+
             }
         }
     }

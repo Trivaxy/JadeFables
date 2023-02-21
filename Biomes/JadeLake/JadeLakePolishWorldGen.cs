a1388022f75d95e4b4c7bc343c3ba0fcf08bdf2c
GraydeeSi <wiseguy0002@gmail.com>
2023-02-21 14:48:46 -0500

    Cherry blossom walls

--- Biomes/JadeLake/JadeLakePolishWorldGen.cs
+++ Biomes/JadeLake/JadeLakePolishWorldGen.cs
@@ -1,5 +1,6 @@
 ﻿using JadeFables.Dusts;
 using JadeFables.Helpers.FastNoise;
+using JadeFables.Tiles.BlossomWall;
 using JadeFables.Tiles.JadeGrass;
 using JadeFables.Tiles.JadeSand;
 using JadeFables.Tiles.JadeWaterfall;
@@ -30,9 +31,13 @@ namespace JadeFables.Biomes.JadeLake
             //Places spring chests
             PlaceJadeChests(worldRect, 40);
 
+            //Places blossom walls
+            BlossomWallPopulation(worldRect, 0.03f, 5f, 3, 5, 7, 10f);
+
             //Places jade grass
             JadeGrassPopulation(worldRect, 0.1f, 5f);
 
+            //Places sand piles
             PlaceJadeSandPiles(worldRect, 5);
         }
 
@@ -56,6 +61,41 @@ namespace JadeFables.Biomes.JadeLake
                         {
                             tileAbove.HasTile = true;
                             tileAbove.TileType = (ushort)ModContent.TileType<JadeGrassTile>();
+                        }
+                    }
+                }
+            }
+        }
+
+        public static void BlossomWallPopulation(Rectangle rect, float threshhold, float noiseFreq, int chance, int heightMin, int heightMax, float noiseFreqCircle)
+        {
+            FastNoise fastnoise = new FastNoise(WorldGen.genRand.Next(0, 10000));
+            for (int i = rect.Left; i < rect.Left + rect.Width; i++)
+            {
+                for (int j = rect.Top + 1; j < rect.Top + rect.Height; j++)
+                {
+                    Tile tileAbove = Framing.GetTileSafely(i, j - 1);
+                    Tile mainTile = Framing.GetTileSafely(i, j);
+
+                    if (!tileAbove.HasTile && mainTile.HasTile && mainTile.TileType == ModContent.TileType<JadeSandTile>())
+                    {
+                        float noiseVal = fastnoise.GetPerlin(i * noiseFreq, j * noiseFreq);
+                        if (noiseVal > threshhold && WorldGen.genRand.NextBool(chance))
+                        {
+                            for (float rad = 0; rad < 6.28f; rad+= 0.03f)
+                            {
+                                float x = i + (float)Math.Cos(rad);
+                                float y = j + (float)Math.Sin(rad);
+                                int height = (int)MathHelper.Lerp(heightMin, heightMax, fastnoise.GetPerlin(x * noiseFreqCircle, y * noiseFreqCircle));
+                                for (int h = 0; h < height; h++)
+                                {
+                                    x = i + (MathF.Cos(rad) * h);
+                                    y =j + (MathF.Sin(rad) * h);
+                                    Tile wallTile = Framing.GetTileSafely((int)x,(int)y);
+                                    if (!wallTile.HasTile)
+                                        wallTile.WallType = (ushort)ModContent.WallType<BlossomWall>();
+                                }
+                            }
                         }
                     }
                 }
---
+++ Tiles/BlossomWall/BlossomWall.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Terraria.ID;
+using static Terraria.ModLoader.ModContent;
+
+namespace JadeFables.Tiles.BlossomWall
+{
+	public class BlossomWall : ModWall
+	{
+        public static Vector2 TileAdj => (Lighting.Mode == Terraria.Graphics.Light.LightMode.Retro || Lighting.Mode == Terraria.Graphics.Light.LightMode.Trippy) ? Vector2.Zero : Vector2.One * 12;
+
+
+        public override void SetStaticDefaults()
+		{
+			Main.wallHouse[Type] = false;
+			WallID.Sets.Conversion.Grass[Type] = true;
+			DustType = DustID.Grass;
+			HitSound = SoundID.Grass;
+			AddMapEntry(new Color(50, 140, 90));
+		}
+
+		public override void PostDraw(int i, int j, SpriteBatch spriteBatch)
+		{
+			if (i > Main.screenPosition.X / 16 && i < Main.screenPosition.X / 16 + Main.screenWidth / 16 && j > Main.screenPosition.Y / 16 && j < Main.screenPosition.Y / 16 + Main.screenHeight / 16)
+			{
+				Texture2D tex = Request<Texture2D>("JadeFables/Tiles/BlossomWall/BlossomWallFlow").Value;
+				var rand = new Random(i * j % 192372);
+
+				float offset = i * j % 6.28f + (float)rand.NextDouble() / 8f;
+				float sin = (float)Math.Sin(Main.GameUpdateCount / 45f + offset);
+
+				spriteBatch.Draw(tex, (new Vector2(i + 0.5f, j + 0.5f) + TileAdj) * 16 + new Vector2(1, 0.5f) * sin * 2.2f - Main.screenPosition,
+				new Rectangle(rand.Next(4) * 26, 0, 24, 24), Lighting.GetColor(i, j), offset + sin * 0.09f, new Vector2(12, 12), 1 + sin / 14f, 0, 0);
+			}
+		}
+	}
+}

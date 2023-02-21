82f638c676796899d0cfc811a84e682e49164f53
GraydeeSi <wiseguy0002@gmail.com>
2023-02-21 18:10:32 -0500

    Cherry blossom wall item

--- Tiles/BlossomWall/BlossomWall.cs
+++ Tiles/BlossomWall/BlossomWall.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using Terraria.ID;
 using static Terraria.ModLoader.ModContent;
+using Terraria.ID;
+using Terraria.GameContent.Creative;
+using Terraria.ModLoader;
 
 namespace JadeFables.Tiles.BlossomWall
 {
@@ -33,4 +36,28 @@ namespace JadeFables.Tiles.BlossomWall
 			}
 		}
 	}
+
+    public class BlossomWallItem : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Cherry Blossom Wall");
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 400;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 12;
+            Item.height = 12;
+            Item.maxStack = 999;
+            Item.useTurn = true;
+            Item.autoReuse = true;
+            Item.useAnimation = 15;
+            Item.useTime = 7;
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.consumable = true;
+            Item.createWall = ModContent.WallType<BlossomWall>(); // The ID of the wall that this item should place when used. ModContent.WallType<T>() method returns an integer ID of the wall provided to it through its generic type argument (the type in angle brackets).
+
+        }
+    }
 }

e97f24b6e070570205dd7e422ce295d0e336c635
GraydeeSi <wiseguy0002@gmail.com>
2023-02-24 20:21:03 -0500

    h

--- Tiles/JadeSandWall/JadeSandWall.cs
+++ Tiles/JadeSandWall/JadeSandWall.cs
@@ -18,7 +18,25 @@ namespace JadeFables.Tiles.JadeSandWall
 			AddMapEntry(new Color(107, 78, 50));
 		}
 
-	}
+        public override void PostDraw(int i, int j, SpriteBatch spriteBatch)
+        {
+            if (i > Main.screenPosition.X / 16 && i < Main.screenPosition.X / 16 + Main.screenWidth / 16 && j > Main.screenPosition.Y / 16 && j < Main.screenPosition.Y / 16 + Main.screenHeight / 16)
+            {
+                Texture2D tex = Request<Texture2D>("JadeFables/Tiles/BlossomWall/BlossomWallFlow").Value;
+                var rand = new Random(i + (j * 1000000));
+
+                if (rand.Next(40) == 2)
+                {
+                    float offset = i * j % 6.28f + (float)rand.NextDouble() / 8f;
+                    float sin = (float)Math.Sin(Main.GameUpdateCount / 45f + offset);
+
+                    spriteBatch.Draw(tex, (new Vector2(i + 0.5f, j + 0.5f) + BlossomWall.BlossomWall.TileAdj) * 16 + new Vector2(1, 0.5f) * sin * 2.2f - Main.screenPosition,
+                    new Rectangle(rand.Next(4) * 26, 0, 24, 24), Lighting.GetColor(i, j), MathF.Sin(offset) + sin * 0.09f, new Vector2(12, 12), 1 + sin / 14f, 0, 0);
+                }
+            }
+        }
+
+    }
 
     public class JadeSandWallItem : ModItem
     {

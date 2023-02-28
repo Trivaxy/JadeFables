85a8cf37e0b2f642b495e921a55b55312f3aae1f
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2023-02-27 17:36:35 -0800

    Jade Sand can now grow bamboo

---
+++ Core/Systems/Edits/BambooEdit.cs
@@ -0,0 +1,53 @@
+﻿using JadeFables.Tiles;
+using MonoMod.Cil;
+
+namespace JadeFables.Core.Systems.Edits {
+    /// <summary>
+    /// IL Edit that allows for other tiles to grow bamboo other than just Jungle Grass.
+    /// </summary>
+    public class BambooEdit : ILoadable {
+        public void Load(Mod mod) {
+            IL.Terraria.WorldGen.CheckBamboo += WorldGen_CheckBamboo;
+            IL.Terraria.WorldGen.PlaceBamboo += WorldGen_PlaceBamboo;
+            IL.Terraria.WorldGen.UpdateWorld_OvergroundTile += WorldGen_UpdateWorld_OvergroundTile;
+        }
+
+        public void Unload() { }
+
+        private void WorldGen_CheckBamboo(ILContext il) {
+            ILCursor c = new ILCursor(il);
+
+            DoSwap(c);
+        }
+
+        private void WorldGen_PlaceBamboo(ILContext il) {
+            ILCursor c = new ILCursor(il);
+
+            DoSwap(c);
+        }
+
+        private void WorldGen_UpdateWorld_OvergroundTile(ILContext il) {
+            // This method has two checks, but the second one is negated.
+            ILCursor c = new ILCursor(il);
+
+            DoSwap(c);
+
+            if (!c.TryGotoNext(i => i.MatchCall<Tile>("get_type")) || !c.TryGotoNext(i => i.MatchLdcI4(TileID.JungleGrass))) {
+                return;
+            }
+
+            c.EmitDelegate<Func<int, int>>(tileType => TileSets.CanGrowBamboo[tileType] ? tileType : TileID.JungleGrass);
+        }
+
+        private void DoSwap(ILCursor c) {
+            // Swaps out the hardcoded Tile.type == 60 to TileSets.CanGrowBamboo[Tile.type]
+            if (!c.TryGotoNext(i => i.MatchCall<Tile>("get_type")) || !c.TryGotoNext(i => i.MatchLdcI4(TileID.JungleGrass))) {
+                return;
+            }
+
+            c.EmitDelegate<Func<int, int>>(SwapDelegate);
+        }
+
+        private int SwapDelegate(int tileType) => TileSets.CanGrowBamboo[tileType] ? TileID.JungleGrass : tileType;
+    }
+}
--- Tiles/TileSets.cs
+++ Tiles/TileSets.cs
@@ -4,10 +4,18 @@
     /// where-in the arrays in this class function for purely this mod's purposes.
     /// </summary>
     public static class TileSets {
+
+        public static SetFactory Factory => TileID.Sets.Factory;
+
         /// <summary>
         /// Whether or not a pre-defined TORCH tile (framing must be IDENTICAL to vanilla torches) has the capability
         /// of counting, and thus triggering, the Torch God event.
         /// </summary>
-        public static bool[] TorchThatTriggersTorchGod = TileID.Sets.Factory.CreateBoolSet(false, TileID.Torches);
+        public static bool[] TorchThatTriggersTorchGod = Factory.CreateBoolSet(false, TileID.Torches);
+
+        /// <summary>
+        /// Set of tiles that can grow bamboo on top of them. Defaults to just Jungle Grass.
+        /// </summary>
+        public static bool[] CanGrowBamboo = Factory.CreateBoolSet(false, TileID.JungleGrass);
     }
 }

700ab3b6096351d1711fc4190983b87bfa068eba
GraydeeSi <wiseguy0002@gmail.com>
2023-03-01 11:11:21 -0500

    Item scaling

--- Items/SpringChestLoot/Chopsticks/Chopsticks.cs
+++ Items/SpringChestLoot/Chopsticks/Chopsticks.cs
@@ -76,6 +76,7 @@ namespace JadeFables.Items.SpringChestLoot.Chopsticks
                     modProj.length = ((float)Math.Sqrt(Math.Pow(modProj.swordTexture.Width, 2) + Math.Pow(modProj.swordTexture.Width, 2)) * item.scale) + 34;
                     modProj.lifeSpan = item.useAnimation;
                     modProj.baseAngle = (Main.MouseWorld - Player.Center).ToRotation();
+                    modProj.itemScale = item.scale;
                 }
 
                 if (item.UseSound.HasValue)
@@ -96,6 +97,7 @@ namespace JadeFables.Items.SpringChestLoot.Chopsticks
         public Texture2D swordTexture;
         public float lifeSpan;
         public float baseAngle;
+        public float itemScale;
 
         public float Progress => 1 - Projectile.timeLeft / (float)lifeSpan;
         public int Direction => (Math.Abs(baseAngle) < Math.PI / 2f) ? 1 : -1;
@@ -138,7 +140,7 @@ namespace JadeFables.Items.SpringChestLoot.Chopsticks
 
             if (swordTexture == null)
                 return false;
-            Main.spriteBatch.Draw(swordTexture, Projectile.Center + new Vector2(0, Owner.gfxOffY) + (34 * Projectile.rotation.ToRotationVector2()) - Main.screenPosition, null, lightColor, (Projectile.rotation + 0.78f) + (Owner.direction == -1 ? 0f : 1.57f), new Vector2(Owner.direction == -1 ? 0 : swordTexture.Width, swordTexture.Height), Projectile.scale, Owner.direction == 1 ? SpriteEffects.FlipHorizontally : SpriteEffects.None, 0f);
+            Main.spriteBatch.Draw(swordTexture, Projectile.Center + new Vector2(0, Owner.gfxOffY) + (34 * Projectile.rotation.ToRotationVector2()) - Main.screenPosition, null, lightColor, (Projectile.rotation + 0.78f) + (Owner.direction == -1 ? 0f : 1.57f), new Vector2(Owner.direction == -1 ? 0 : swordTexture.Width, swordTexture.Height), Projectile.scale * itemScale, Owner.direction == 1 ? SpriteEffects.FlipHorizontally : SpriteEffects.None, 0f);
             return false;
         }
 

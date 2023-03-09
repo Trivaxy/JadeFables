2fc6c358e184923838ef48bac821fc10204ff66c
GraydeeSi <wiseguy0002@gmail.com>
2023-03-09 12:58:10 -0500

    Lotus fiber material

---
+++ Items/Lotus/LotusFiber/LotusFiber.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Microsoft.Xna.Framework;
+
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+using JadeFables.Dusts;
+
+namespace JadeFables.Items.Lotus.LotusFiber
+{
+    public class LotusFiber : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Lotus Fiber");
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 24;
+            Item.height = 24;
+
+            Item.maxStack = 999;
+            Item.value = 0;
+            Item.rare = ItemRarityID.White;
+        }
+    }
+}

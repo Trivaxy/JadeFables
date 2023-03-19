2f2df6e4991dd68c628166d83d2385eb4ff13690
GraydeeSi <wiseguy0002@gmail.com>
2023-03-19 00:44:07 -0400

    Fixed worldgen edit

--- Core/Systems/Edits/WorldgenEdit.cs
+++ Core/Systems/Edits/WorldgenEdit.cs
@@ -4,32 +4,52 @@ using MonoMod.Cil;
 
 namespace JadeFables.Core.Systems.Edits {
     /// <summary>
-    /// IL Edit that allows for other tiles to grow bamboo other than just Jungle Grass.
+    /// Detours that disallow certain structures from spawning in the biome
     /// </summary>
-    public class MinecartEdit : ILoadable {
+    public class WorldgenEdit : ILoadable {
         public void Load(Mod mod) {
             On.Terraria.GameContent.Generation.TrackGenerator.IsLocationInvalid += TrackGenerator_IsLocationInvalid;
+            On.Terraria.GameContent.Biomes.MiningExplosivesBiome.Place += MiningExplosivesBiome_Place;
+        }
+
+        private bool MiningExplosivesBiome_Place(On.Terraria.GameContent.Biomes.MiningExplosivesBiome.orig_Place orig, Terraria.GameContent.Biomes.MiningExplosivesBiome self, Point origin, Terraria.WorldBuilding.StructureMap structures)
+        {
+            if (ValidPlacement_Explosives(origin.X,origin.Y))
+                return orig(self, origin, structures);
+            return false;
         }
 
         private bool TrackGenerator_IsLocationInvalid(On.Terraria.GameContent.Generation.TrackGenerator.orig_IsLocationInvalid orig, int x, int y)
         {
-            if (ValidPlacement(x, y))
+            if (ValidPlacement_Minecarts(x, y))
                 return orig(x, y);
-            return false;
+            return true;
         }
 
         public void Unload() 
         {
             On.Terraria.GameContent.Generation.TrackGenerator.IsLocationInvalid -= TrackGenerator_IsLocationInvalid;
+            On.Terraria.GameContent.Biomes.MiningExplosivesBiome.Place -= MiningExplosivesBiome_Place;
         }
 
-        public bool ValidPlacement(int x, int y)
+        public bool ValidPlacement_Minecarts(int x, int y)
         {
-            foreach(Rectangle rect in JadeLakeWorldGen.LowerIslandRects)
+            foreach (Rectangle rect in JadeLakeWorldGen.LowerIslandRects)
             {
                 if (ContainsCoordinates(rect, x, y))
                     return false;
             }
+
+            foreach (Rectangle rect in JadeLakeWorldGen.PagodaRects)
+            {
+                if (ContainsCoordinates(rect, x, y))
+                    return false;
+            }
+            return true;
+        }
+
+        public bool ValidPlacement_Explosives(int x, int y)
+        {
 
             foreach (Rectangle rect in JadeLakeWorldGen.PagodaRects)
             {

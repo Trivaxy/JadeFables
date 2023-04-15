e7179ef96137e1acf5ef9df92008fee44336b5f4
GraydeeSi <wiseguy0002@gmail.com>
2023-04-14 20:57:54 -0400

    Jade mantis WIP

---
+++ NPCs/JadeMantis/JadeMantis.cs
@@ -0,0 +1,340 @@
+﻿//TODO:
+//Banners
+//Bestiary
+//Balance
+//Gores
+//Thrust attack
+//Hiding behavior
+//Hiding spot pick
+//Hitsound
+//Deathsound
+//Spear throwing sound
+//Spear landing sound
+//Money dropping
+//Better pop out
+//Make it draw behind tiles
+//Adjust damage for expert and master
+//Spear sprite
+//Throwing animation and offset
+//Contact damage only when thrusting
+//Spear impact dust
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.Graphics.Effects;
+using System.Collections.Generic;
+
+using Terraria.DataStructures;
+using Terraria.GameContent;
+
+using Terraria.Audio;
+
+using System;
+using System.Linq;
+using static Terraria.ModLoader.ModContent;
+using JadeFables.Core;
+using JadeFables.Helpers;
+using static System.Formats.Asn1.AsnWriter;
+using JadeFables.Biomes.JadeLake;
+using Terraria.GameContent.ItemDropRules;
+using Terraria.GameContent.Bestiary;
+using Humanizer;
+using System.Reflection;
+using JadeFables.Tiles.Banners;
+using static Humanizer.In;
+
+namespace JadeFables.NPCs.JadeMantis
+{
+    internal class JadeMantis : ModNPC
+    {
+        private enum AttackPhase
+        {
+            JustSpawned = 0,
+            Hiding = 1,
+            Idle = 2,
+            Swooping = 3,
+            Throwing = 4,
+        }
+
+        private AttackPhase attackPhase = AttackPhase.JustSpawned;
+        private Player target => Main.player[NPC.target];
+
+        private int XFRAMES = 1;
+        private int xFrame = 0;
+        private int yFrame = 0;
+        private int frameCounter = 0;
+
+        private float bobCounter = 0f;
+
+        int attackTimer = 0;
+
+        private Vector2 movementTarget = Vector2.Zero;
+        public Vector2 oldPosition = Vector2.Zero;
+
+        private Vector2 spearVel = Vector2.Zero;
+
+        public override void SetStaticDefaults()
+        {
+            Main.npcFrameCount[NPC.type] = 4;
+        }
+
+        public override void SetDefaults()
+        {
+            NPC.width = 64;
+            NPC.height = 64;
+            NPC.damage = 0;
+            NPC.defense = 5;
+            NPC.lifeMax = 300;
+            NPC.value = 10f;
+            NPC.knockBackResist = 0.4f;
+            NPC.HitSound = SoundID.NPCHit21 with { Pitch = -0.45f };
+            NPC.DeathSound = SoundID.NPCDeath53 with { Pitch = -0.6f};
+            NPC.noGravity = true;
+        }
+
+        public override void SetBestiary(BestiaryDatabase database, BestiaryEntry bestiaryEntry)
+        {
+            bestiaryEntry.Info.AddRange(new IBestiaryInfoElement[]
+            {
+                JadeSpawnConditions.JadeSprings,
+                new FlavorTextBestiaryInfoElement("Mantis. I am gamer")
+            });
+        }
+
+        public override void OnHitByProjectile(Projectile projectile, NPC.HitInfo hit, int damageDone)
+        {
+            if (attackPhase == AttackPhase.Hiding)
+            {
+                attackPhase = AttackPhase.Idle;
+            }
+        }
+
+        public override void OnHitByItem(Player player, Item item, NPC.HitInfo hit, int damageDone)
+        {
+            if (attackPhase == AttackPhase.Hiding)
+            {
+                attackPhase = AttackPhase.Idle;
+            }
+        }
+
+        public override void AI()
+        {
+            NPC.TargetClosest(true);
+
+            switch (attackPhase)
+            {
+                case AttackPhase.JustSpawned:
+                    NPC.Center = FindSpot();
+                    attackPhase = AttackPhase.Hiding;
+                    break;
+                case AttackPhase.Hiding:
+                    HidingBehavior();
+                    break;
+                case AttackPhase.Idle:
+                    IdleBehavior();
+                    break;
+                case AttackPhase.Swooping:
+                    SwoopingBehavior();
+                    break;
+                case AttackPhase.Throwing:
+                    ThrowingBehavior();
+                    break;
+            }
+        }
+
+        private Vector2 FindSpot()
+        {
+            return NPC.Center;
+        }
+
+        private void HidingBehavior()
+        {
+            NPC.velocity = Vector2.Zero;
+
+            if (NPC.Distance(target.Center) < 200)
+            {
+                attackPhase = AttackPhase.Idle;
+            }
+        }
+
+        private void IdleBehavior()
+        {
+            attackTimer++;
+            bobCounter += 0.2f;
+            if (GoToPos(movementTarget, oldPosition) || movementTarget == Vector2.Zero)
+            {
+                oldPosition = NPC.Center;
+                movementTarget = Main.rand.NextVector2Circular(500, 400);
+                movementTarget.Y *= -Math.Sign(movementTarget.Y);
+                movementTarget += target.Center;
+                if (attackTimer > 200)
+                {
+                    attackTimer = 0;
+                    xFrame = 0;
+                    yFrame = 0;
+                    frameCounter = 0;
+                    Vector2 pos = NPC.Center;
+                    spearVel = pos.DirectionTo(target.Center) * 40;
+                    attackPhase = AttackPhase.Throwing;
+                }
+            }
+        }
+
+        private void SwoopingBehavior()
+        {
+
+        }
+
+        private void ThrowingBehavior()
+        {
+            NPC.velocity = Vector2.Zero;
+            if (frameCounter == 0 && yFrame == 3)
+            {
+                Vector2 pos = NPC.Center;
+                Projectile.NewProjectileDirect(NPC.GetSource_FromAI(), pos, spearVel, ModContent.ProjectileType<JadeMantisSpear>(), NPC.damage, 3);
+            }
+        }
+
+        /// <summary>
+		/// Attempts to navigate to the given position 
+		/// </summary>
+		/// <param name="pos"> the destination for the NPC </param>
+		/// <param name="oldPos"> the point at which the NPC started it's journey </param>
+		/// <returns> If the enemy has reached it's destination </returns>
+		private bool GoToPos(Vector2 pos, Vector2 oldPos)
+        {
+            float distance = pos.X - oldPos.X;
+            float progress = MathHelper.Clamp((NPC.Center.X - oldPos.X) / distance, 0, 1);
+
+            Vector2 dir = NPC.DirectionTo(pos);
+
+            if (NPC.Distance(pos) > 7 && !NPC.collideY && !NPC.collideX)
+            {
+                NPC.velocity = dir * ((float)Math.Sin(progress * 3.14f) + 0.1f) * 5;
+                NPC.velocity.Y += (float)Math.Cos(bobCounter) * 0.45f;
+                return false;
+            }
+
+            NPC.velocity.Y = (float)Math.Cos(bobCounter) * 0.45f;
+            return true;
+        }
+
+        public override bool PreDraw(SpriteBatch spriteBatch, Vector2 screenPos, Color drawColor)
+        {
+            Texture2D mainTex = ModContent.Request<Texture2D>(Texture).Value;
+
+            int frameWidth = mainTex.Width / XFRAMES;
+            int frameHeight = mainTex.Height / Main.npcFrameCount[NPC.type];
+            Rectangle frameBox = new Rectangle(xFrame * frameWidth, yFrame * frameHeight, frameWidth, frameHeight);
+
+            SpriteEffects effects = SpriteEffects.None;
+            Vector2 origin = new Vector2(frameWidth, frameHeight) / 2;
+
+            if (NPC.spriteDirection != 1)
+            {
+                effects = SpriteEffects.FlipHorizontally;
+                origin.X = frameWidth - origin.X;
+            }
+            Vector2 slopeOffset = new Vector2(0, NPC.gfxOffY);
+            Main.spriteBatch.Draw(mainTex, slopeOffset + NPC.Center - screenPos, frameBox, drawColor, NPC.rotation, origin, NPC.scale, effects, 0f);
+            return false;
+        }
+
+        public override void FindFrame(int frameHeight)
+        {
+            if (NPC.IsABestiaryIconDummy || attackPhase == AttackPhase.Idle)
+            {
+                xFrame = 0;
+                frameCounter++;
+                if (frameCounter > 4)
+                {
+                    frameCounter = 0;
+                    yFrame++;
+                }
+                yFrame %= 4;
+            }
+
+            if (attackPhase == AttackPhase.Throwing)
+            {
+                xFrame = 0;
+                frameCounter++;
+                if (frameCounter > 30)
+                {
+                    frameCounter = 0;
+                    yFrame++;
+                }
+                if (yFrame >= 4)
+                {
+                    yFrame = 0;
+                    xFrame = 0;
+                    attackPhase = AttackPhase.Idle;
+                }
+            }
+        }
+
+        public override float SpawnChance(NPCSpawnInfo spawnInfo) => !spawnInfo.Water && !spawnInfo.PlayerSafe && spawnInfo.Player.InModBiome(ModContent.GetInstance<JadeLakeBiome>()) ? 4f : 0f;
+    }
+
+    public class JadeMantisSpear : ModProjectile
+    {
+        public bool stuck = false;
+
+        public float opacity = 1;
+
+        public float shakeTimer = 1;
+
+        float oldRot = 0;
+        public override void SetDefaults()
+        {
+            Projectile.width = 12;
+            Projectile.height = 12;
+            Projectile.friendly = false;
+            Projectile.hostile = true;
+            Projectile.tileCollide = true;
+            Projectile.hide = true;
+        }
+
+        public override void DrawBehind(int index, List<int> behindNPCsAndTiles, List<int> behindNPCs, List<int> behindProjectiles, List<int> overPlayers, List<int> overWiresUI)
+        {
+            behindNPCsAndTiles.Add(index);
+        }
+
+        public override void AI()
+        {
+            if (stuck)
+            {
+                opacity -= 0.01f;
+                if (opacity <= 0)
+                    Projectile.active = false;
+
+                if (shakeTimer > 0)
+                    shakeTimer -= 0.1f;
+                Projectile.rotation = oldRot + (0.3f * MathF.Sin(shakeTimer * 6.28f) * shakeTimer);
+            }
+            else
+            {
+                Projectile.rotation = Projectile.velocity.ToRotation() + 1.57f;
+            }
+        }
+
+        public override bool OnTileCollide(Vector2 oldVelocity)
+        {
+            oldRot = Projectile.rotation;
+            Projectile.velocity = Vector2.Zero;
+            stuck = true;
+            Projectile.hostile = false;
+            Projectile.position += oldVelocity;
+            return false;
+        }
+
+        public override bool PreDraw(ref Color lightColor)
+        {
+            Texture2D tex = ModContent.Request<Texture2D>(Texture).Value;
+            Vector2 origin = new Vector2(tex.Width / 2, 24);
+            Main.spriteBatch.Draw(tex, Projectile.Center - Main.screenPosition, null, lightColor * opacity, Projectile.rotation, origin, Projectile.scale, SpriteEffects.None, 0f);
+            return false;
+        }
+    }
+}
